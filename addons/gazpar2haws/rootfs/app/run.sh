22da915eee1b4bfdc63538deb93d3eecb42cc0ff
Stéphane Senart <>
2025-02-02 19:00:27 +0100

    [#31] Add cost computation

--- addons/gazpar2haws/rootfs/app/run.sh
+++ addons/gazpar2haws/rootfs/app/run.sh
@@ -24,7 +24,7 @@ ENERGY_TAXES_JSON=$(jq --raw-output '.energy_taxes // empty' $CONFIG_PATH)
 
 PRICING_JSON="{ 'pricing': { 'vat': $VAT_JSON , 'consumption_prices': $CONSUMPTION_PRICES_JSON, 'subscription_prices': $SUBSCRIPTION_PRICES_JSON, 'transport_prices': $TRANSPORT_PRICES_JSON, 'energy_taxes': $ENERGY_TAXES_JSON } }"
 
-PRICING_CONGIG=$(echo $PRICING_JSON | yq -P)
+PRICING_CONFIG=$(echo $PRICING_JSON | yq -P)
 
 # Home Assistant configuration for Add-on
 HOMEASSISTANT_HOST=supervisor
@@ -33,15 +33,15 @@ HOMEASSISTANT_ENDPOINT=/core/websocket
 HOMEASSISTANT_TOKEN=${SUPERVISOR_TOKEN}
 
 # Display environment variables
-bashio::log.info "GRDF_CONFIG: ${GRDF_CONFIG}"
-bashio::log.info "PRICING_CONGIG: ${PRICING_CONGIG}"
+# bashio::log.info "GRDF_CONFIG: ${GRDF_CONFIG}"
+# bashio::log.info "PRICING_CONFIG: ${PRICING_CONFIG}"
 bashio::log.info "HOMEASSISTANT_HOST: ${HOMEASSISTANT_HOST}"
 bashio::log.info "HOMEASSISTANT_PORT: ${HOMEASSISTANT_PORT}"
 bashio::log.info "HOMEASSISTANT_ENDPOINT: ${HOMEASSISTANT_ENDPOINT}"
 bashio::log.info "HOMEASSISTANT_TOKEN: ${HOMEASSISTANT_TOKEN}"
 
 # Export environment variables
-export GRDF_CONFIG PRICING_CONGIG HOMEASSISTANT_HOST HOMEASSISTANT_PORT HOMEASSISTANT_ENDPOINT HOMEASSISTANT_TOKEN
+export GRDF_CONFIG PRICING_CONFIG HOMEASSISTANT_HOST HOMEASSISTANT_PORT HOMEASSISTANT_ENDPOINT HOMEASSISTANT_TOKEN
 
 # Instantiate the template config
 if [ ! -e /app/config/configuration.yaml ]; then
